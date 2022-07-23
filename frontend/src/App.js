18e8f24ba8828153cb70a6239bfaac3fbd0ec77c
Kokutse Djoguenou <kokutse@Kokutses-MacBook-Pro.local>
2022-07-22 21:40:25 -0400

    Added UI template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
+import Dashboard from './components/Dashboard';
+
 function App() {
     return (
         <div className="App">
-            <h1>Welcome</h1>
+            <Dashboard />
         </div>
     );
 }
